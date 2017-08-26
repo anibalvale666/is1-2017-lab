56b5d11414ff7a634eb643757dc67da694108d57
anibal <anibal.valenzuelacsp.edu.pe>
2017-08-26 14:14:43 -0500

    correcion del hla mundo y ignoramos archivos binarios

--- Hola.java
+++ Hola.java
@@ -1,5 +1,5 @@
 public class Hola{
-	public static void main(String.. args){
-		 system.out.println("Hola mundo")
+	public static void main(String[] args){
+		 System.out.println("hola mundo");
 	}
 }
